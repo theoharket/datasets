66a7ad5753bde5642f51d2679f075ecf0e1f4908
theoharket <55239037+theoharket@users.noreply.github.com>
2023-03-21 15:46:08 +0100

    Update SMM636TheodorHarket_Task4_Functions.R

--- SMM636TheodorHarket_Task4_Functions.R
+++ SMM636TheodorHarket_Task4_Functions.R
@@ -4,8 +4,10 @@ myFDA <- function(X, y) {
   
   
   #Calculate the within-class scatter matrix for both the classes
-  SW0 = t(X[y == 0,] - colMeans(X[y == 0,])) %*% (X[y == 0,] - colMeans(X[y == 0,]))
-  SW1 = t(X[y == 1,] - colMeans(X[y == 1,])) %*% (X[y == 1,] - colMeans(X[y == 1,]))
+  SW0 = (X[y == 0,] - colMeans(X[y == 0,])
+  SW1 = (X[y == 1,] - colMeans(X[y == 1,])
+  SW0 = t(SW0) %*% (Sw0)
+  SW1 = t(SW1) %*% (SW1)
   
   #Calculating the scatter matrix for both classes
   S_W = SW0 + SW1
@@ -13,10 +15,11 @@ myFDA <- function(X, y) {
   #Calculating the difference in means to get the direction of class mean difference
   class_means = colMeans(X[y == 1,]) - colMeans(X[y == 0,])
   
-  
-  # Calculate the linear discriminant
+
+  #Taking the inverse of the weights, multiplying it with the difference in class means
   w = solve(S_W) %*% class_means
   
+  #Returning the corresponding weights
   return(w)
 }
 

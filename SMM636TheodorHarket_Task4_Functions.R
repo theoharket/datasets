a36a57fd1b2641261d65f25a915287b7e392466f
theoharket <55239037+theoharket@users.noreply.github.com>
2023-03-22 19:56:32 +0100

    Update SMM636TheodorHarket_Task4_Functions.R

--- SMM636TheodorHarket_Task4_Functions.R
+++ SMM636TheodorHarket_Task4_Functions.R
@@ -2,18 +2,18 @@
 #Fisher discrimant analysis-function
 myFDA <- function(x, y) {
   
-  #Since it is unclear in the task description whether we want the Fisher's Linear Discriminant Analysis, or Fisher's Discriminant Analysis.
-  #This function is implementing the Fisher's Linear Discriminant Analysis.
+  #I think it is a bit unclear in the task description whether we want the Fisher's Linear Discriminant Analysis, or Fisher's Discriminant Analysis, but I chose
+  #to implement the Fisher's Linear Discriminant Analysis due to usage of the word FDA and linear discriminant in the description.
   
   #It's important to note that this implementation assumes that the covariance matrix is the same for both classes, 
-  #the equal covariance assumption. If this assumption does not hold, this function will not have the correct implementation.
+  #known as the equal covariance assumption. If this assumption does not hold, this function will not have the correct implementation.
   
   #Steps in this function:
-  #1. Calculate the within-class scatter matrix for both the classes by subtracting every column for both of the classes from the mean of the feature
-  #2. Multiplying the corresponding SW0 and SW1 separately, and then adding them to get the scatter matrix combined for both classes. 
+  #1. Calculating the within-class scatter matrix for both the classes by subtracting the value for every feature for all rows divided into classes, from the mean of the feature
+  #2. Multiplying the corresponding SW0 and SW1 separately, adding them to get the scatter matrix combined for both classes. 
           #Giving me a pxp matrix
-  #3. Calculating the difference in means to get the direction of class mean difference
-  #4. Taking the inverse of the weights, multiplying it with the difference in class means
+  #3. Calculating the difference in means to get the direction of the class mean difference
+  #4. Multiplying the inverse of the weights with the difference in class means
   #5. Returning the weights
   
   

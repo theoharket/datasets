5f4cfe46ae4bf796ff67fb50b26cf7d37b9089fd
theoharket <55239037+theoharket@users.noreply.github.com>
2023-03-22 19:39:46 +0100

    Update SMM636TheodorHarket_Task4_Functions.R

--- SMM636TheodorHarket_Task4_Functions.R
+++ SMM636TheodorHarket_Task4_Functions.R
@@ -1,20 +1,28 @@
 
 #Fisher discrimant analysis-function
-myFDA <- function(X, y) {
+myFDA <- function(x, y) {
+  
+  #Since it is unclear whether we want the Fisher's Linear Discriminant Analysis, or Fisher's Discriminant Analysis due to the task description being unclear
+  #where we assume that the normality assumption is fulfilled, where we expect tjat tje 
+  
+  #Steps in this function:
+  #1. Calculate the within-class scatter matrix for both the classes by subtracting every column for both of the classes from the mean of the feature
+  #2. Multiplying the corresponding SW0 and SW1 separately, and then adding them to get the scatter matrix combined for both classes. 
+          #Giving me a pxp matrix
+  #3. Calculating the difference in means to get the direction of class mean difference
   
   
-  #Calculate the within-class scatter matrix for both the classes
-  SW0 = (X[y == 0,] - colMeans(X[y == 0,]))
-  SW1 = (X[y == 1,] - colMeans(X[y == 1,]))
-  
+  #1.
+  SW0 = (x[y == 0,] - colMeans(x[y == 0,]))
+  SW1 = (x[y == 1,] - colMeans(x[y == 1,]))
   SW0 = (t(SW0) %*% (SW0))
   SW1 = (t(SW1) %*% (SW1))
   
-  #Calculating the scatter matrix for both classes
+  #2.
   S_W = SW0 + SW1
   
   #Calculating the difference in means to get the direction of class mean difference
-  class_means = colMeans(X[y == 1,]) - colMeans(X[y == 0,])
+  class_means = colMeans(x[y == 1,]) - colMeans(x[y == 0,])
   
 
   #Taking the inverse of the weights, multiplying it with the difference in class means

f32ff66e12d04b3d25aa6d96d14180088974b887
theoharket <55239037+theoharket@users.noreply.github.com>
2023-03-23 15:13:30 +0100

    Update SMM636TheodorHarket_Task4_Functions.R

--- SMM636TheodorHarket_Task4_Functions.R
+++ SMM636TheodorHarket_Task4_Functions.R
@@ -18,6 +18,8 @@ myFDA <- function(x, y) {
   
   
   #1.
+  print(dim(x[y == 0,]))
+  print(colMeans(x[y == 0,]))
   SW0 = (x[y == 0,] - colMeans(x[y == 0,]))
   SW1 = (x[y == 1,] - colMeans(x[y == 1,]))
   SW0 = (t(SW0) %*% (SW0))
